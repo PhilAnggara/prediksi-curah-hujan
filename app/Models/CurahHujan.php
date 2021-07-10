2731af78dbc5abe1279ab5497ec5eed1565ad8f0
Angga Bawole <bawoleangga03@gmail.com>
2021-07-11 00:07:52 +0800

    initial commit

---
+++ app/Models/CurahHujan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CurahHujan extends Model
+{
+    use HasFactory;
+}
